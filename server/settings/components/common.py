191a51e1c364dff773c5b02431085e062c471932
Dzyundzya Alexander <dzyundzya.aa@yandex.ru>
2025-09-26 22:36:59 +0300

    add swagger file

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -68,23 +68,29 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('POSTGRES_DB'),
+#         'USER': config('POSTGRES_USER'),
+#         'PASSWORD': config('POSTGRES_PASSWORD'),
+#         'HOST': config('DJANGO_DATABASE_HOST'),
+#         'PORT': config('DJANGO_DATABASE_PORT', cast=int),
+#         'CONN_MAX_AGE': config('CONN_MAX_AGE', cast=int, default=60),
+#         'OPTIONS': {
+#             'connect_timeout': 10,
+#             'options': '-c statement_timeout=15000ms',
+#             # consider using 'isolation_level' set to 'serializable'
+#         },
+#     },
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': config('DJANGO_DATABASE_HOST'),
-        'PORT': config('DJANGO_DATABASE_PORT', cast=int),
-        'CONN_MAX_AGE': config('CONN_MAX_AGE', cast=int, default=60),
-        'OPTIONS': {
-            'connect_timeout': 10,
-            'options': '-c statement_timeout=15000ms',
-            # consider using 'isolation_level' set to 'serializable'
-        },
-    },
+        'ENGINE': 'django.db.backends.sqlite3',  # Database engine
+        'NAME': BASE_DIR / 'db.sqlite3',  # Database file path
+    }
 }
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
